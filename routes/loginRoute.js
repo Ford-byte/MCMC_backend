7cb39929812a2d37151013c078d90c5962f132e8
Ford-byte <165389462+Aihsseikbahala@users.noreply.github.com>
2024-06-27 13:54:18 +0800

    06/27/24  otp

--- routes/loginRoute.js
+++
@@ -1,9 +0,0 @@
-const express = require('express')
-
-const router = express.Router()
-
-const { verify } = require('../controller/loginController')
-
-router.post('/login',verify);
-
-module.exports = { router }
